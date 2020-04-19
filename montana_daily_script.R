0683c3f8fe081bc51ec1e0e78f1b5663929db080
ewalk86 <ethanwalker86@gmail.com>
2020-04-19 13:43:25 -0600

    state data pull updates

--- montana_daily_script.R
+++ montana_daily_script.R
@@ -18,7 +18,7 @@ library(epitrix)
 library(jsonlite)
 library(httr)
 library(rlist)
-library(taskscheduleR)
+library(Rsftp)
 
 
 # Query and prep data
@@ -100,11 +100,23 @@ state_results <- estimate_R(state_incidence, method="parametric_si",
 
 state_plot <- estimate_R_plots(state_results, what = "R")
 
-ggsave("C:/R/covid19/state_plot.png")
+ggsave("C:/R/covid19/state_daily_results/state_plot.png")
 
 colomns <- c(1:4, 8)
 state_r <- (state_results$R[,colomns]) %>% 
-   mutate(region = "state")
+   mutate(region = "state") %>% 
+   rename(mean_r = `Mean(R)`,
+          sd_r = `Std(R)`,
+          median_r = `Median(R)`)
+
+state_dates <- as.data.frame(state_results$dates)
+state_i <- as.data.frame(state_results$I)
+state_dates_new <- cbind(state_dates, state_i) %>% 
+   rename(dates = 1,
+          incidence = 2) %>% 
+   mutate(dates = ymd(dates))
+state_dates_new <- state_dates_new[-(1:7), 1:2]
+state_r_clean <- cbind(state_r, state_dates_new)
 
 
 ## Reg1 results
@@ -119,11 +131,23 @@ reg1_results <- estimate_R(reg1_incidence, method="parametric_si",
 
 reg1_plot <- estimate_R_plots(reg1_results, what = "R")
 
-ggsave("C:/R/covid19/reg1_plot.png")
+ggsave("C:/R/covid19/state_daily_results/reg1_plot.png")
 
 colomns <- c(1:4, 8)
 reg1_r <- (reg1_results$R[,colomns]) %>% 
-   mutate(region = "1")
+   mutate(region = "1") %>% 
+   rename(mean_r = `Mean(R)`,
+          sd_r = `Std(R)`,
+          median_r = `Median(R)`)
+
+reg1_dates <- as.data.frame(reg1_results$dates)
+reg1_i <- as.data.frame(reg1_results$I)
+reg1_dates_new <- cbind(reg1_dates, reg1_i) %>% 
+   rename(dates = 1,
+          incidence = 2) %>% 
+   mutate(dates = ymd(dates))
+reg1_dates_new <- reg1_dates_new[-(1:7), 1:2]
+reg1_r_clean <- cbind(reg1_r, reg1_dates_new)
 
 
 ## Reg2 results
@@ -138,11 +162,23 @@ reg2_results <- estimate_R(reg2_incidence, method="parametric_si",
 
 reg2_plot <- estimate_R_plots(reg2_results, what = "R")
 
-ggsave("C:/R/covid19/reg2_plot.png")
+ggsave("C:/R/covid19/state_daily_results/reg2_plot.png")
 
 colomns <- c(1:4, 8)
 reg2_r <- (reg2_results$R[,colomns]) %>% 
-   mutate(region = "2")
+   mutate(region = "2") %>% 
+   rename(mean_r = `Mean(R)`,
+          sd_r = `Std(R)`,
+          median_r = `Median(R)`)
+
+reg2_dates <- as.data.frame(reg2_results$dates)
+reg2_i <- as.data.frame(reg2_results$I)
+reg2_dates_new <- cbind(reg2_dates, reg2_i) %>% 
+   rename(dates = 1,
+          incidence = 2) %>% 
+   mutate(dates = ymd(dates))
+reg2_dates_new <- reg2_dates_new[-(1:7), 1:2]
+reg2_r_clean <- cbind(reg2_r, reg2_dates_new)
 
 
 ## Reg3 results
@@ -157,11 +193,23 @@ reg3_results <- estimate_R(reg3_incidence, method="parametric_si",
 
 reg3_plot <- estimate_R_plots(reg3_results, what = "R")
 
-ggsave("C:/R/covid19/reg3_plot.png")
+ggsave("C:/R/covid19/state_daily_results/reg3_plot.png")
 
 colomns <- c(1:4, 8)
 reg3_r <- (reg3_results$R[,colomns]) %>% 
-   mutate(region = "3")
+   mutate(region = "3") %>% 
+   rename(mean_r = `Mean(R)`,
+          sd_r = `Std(R)`,
+          median_r = `Median(R)`)
+
+reg3_dates <- as.data.frame(reg3_results$dates)
+reg3_i <- as.data.frame(reg3_results$I)
+reg3_dates_new <- cbind(reg3_dates, reg3_i) %>% 
+   rename(dates = 1,
+          incidence = 2) %>% 
+   mutate(dates = ymd(dates))
+reg3_dates_new <- reg3_dates_new[-(1:7), 1:2]
+reg3_r_clean <- cbind(reg3_r, reg3_dates_new)
 
 
 ## Reg4 results
@@ -176,11 +224,23 @@ reg4_results <- estimate_R(reg4_incidence, method="parametric_si",
 
 reg4_plot <- estimate_R_plots(reg4_results, what = "R")
 
-ggsave("C:/R/covid19/reg4_plot.png")
+ggsave("C:/R/covid19/state_daily_results/reg4_plot.png")
 
 colomns <- c(1:4, 8)
 reg4_r <- (reg4_results$R[,colomns]) %>% 
-   mutate(region = "4")
+   mutate(region = "4") %>% 
+   rename(mean_r = `Mean(R)`,
+          sd_r = `Std(R)`,
+          median_r = `Median(R)`)
+
+reg4_dates <- as.data.frame(reg4_results$dates)
+reg4_i <- as.data.frame(reg4_results$I)
+reg4_dates_new <- cbind(reg4_dates, reg4_i) %>% 
+   rename(dates = 1,
+          incidence = 2) %>% 
+   mutate(dates = ymd(dates))
+reg4_dates_new <- reg4_dates_new[-(1:7), 1:2]
+reg4_r_clean <- cbind(reg4_r, reg4_dates_new)
 
 
 ## Reg5 results
@@ -195,17 +255,76 @@ reg5_results <- estimate_R(reg5_incidence, method="parametric_si",
 
 reg5_plot <- estimate_R_plots(reg5_results, what = "R")
 
-ggsave("C:/R/covid19/reg5_plot.png")
+ggsave("C:/R/covid19/state_daily_results/reg5_plot.png")
 
 colomns <- c(1:4, 8)
 reg5_r <- (reg5_results$R[,colomns]) %>% 
-   mutate(region = "5")
+   mutate(region = "5") %>% 
+   rename(mean_r = `Mean(R)`,
+          sd_r = `Std(R)`,
+          median_r = `Median(R)`)
+
+reg5_dates <- as.data.frame(reg5_results$dates)
+reg5_i <- as.data.frame(reg5_results$I)
+reg5_dates_new <- cbind(reg5_dates, reg5_i) %>% 
+   rename(dates = 1,
+          incidence = 2) %>% 
+   mutate(dates = ymd(dates))
+reg5_dates_new <- reg5_dates_new[-(1:7), 1:2]
+reg5_r_clean <- cbind(reg5_r, reg5_dates_new)
 
 
 
 # Bind files and save
-all_regions_r <- rbind(state_r, reg1_r, reg2_r, reg3_r, reg4_r, reg5_r)
+all_regions_r <- rbind(state_r_clean, reg1_r_clean, reg2_r_clean, reg3_r_clean, 
+                       reg4_r_clean, reg5_r_clean)
 
-write_csv(all_regions_r, "C:/R/covid19/all_regions_r.csv")
+write_csv(all_regions_r, "C:/R/covid19/state_daily_results/all_regions_r.csv")
+
+
+
+# Send files to the sftp server
+# host = 'elbastion.dbs.umt.edu'
+# port = 22
+# username = 'celftp'
+# password  = 'celftp'
+# remotepath = '/celFtpFiles/covid19/Rt/incoming/'
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/all_regions_r.csv",
+           "C:/R/covid19/state_daily_results/all_regions_r.csv")
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/state_plot.png",
+           "C:/R/covid19/state_daily_results/state_plot.png")
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/reg1_plot.png",
+           "C:/R/covid19/state_daily_results/reg1_plot.png")
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/reg2_plot.png",
+           "C:/R/covid19/state_daily_results/reg2_plot.png")
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/reg3_plot.png",
+           "C:/R/covid19/state_daily_results/reg3_plot.png")
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/reg4_plot.png",
+           "C:/R/covid19/state_daily_results/reg4_plot.png")
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/reg5_plot.png",
+           "C:/R/covid19/state_daily_results/reg5_plot.png")
+
+
+
+# Test to see if I can pull file back from server
+
+#sftpDownload("elbastion.dbs.umt.edu", "celftp", "celftp",
+#           "/celFtpFiles/covid19/Rt/incoming/reg5_plot.png",
+#           "C:/R/covid19/reg5_plot.png")
+
 
 
