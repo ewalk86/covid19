941a68f148527b168a452a3cb4d0a73bdc70e5c0
ewalk86 <ethanwalker86@gmail.com>
2020-05-13 09:36:08 -0600

    new state/county hospitalization data

--- montana_daily_script.R
+++ montana_daily_script.R
@@ -1,6 +1,6 @@
 # Pull daily updated State of Montana covid data, estimate R, save results/plots
 # Ethan Walker
-# 21 April 2020
+# 13 May 2020
 
 library(tidyverse)
 library(readxl)
@@ -19,10 +19,10 @@ library(jsonlite)
 library(httr)
 library(rlist)
 library(Rsftp)
-library(sftp)
+#library(sftp)
 
 
-# Query and prep data
+############## Query and prep data
 query_url <- "https://services.arcgis.com/qnjIrwR8z5Izc0ij/ArcGIS/rest/services/COVID_Cases_Production_View/FeatureServer/2/query?where=1%3D1&objectIds=&time=&resultType=none&outFields=OBJECTID%2C+Case_No%2C+Date_Reported_to_CDEpi%2C+County%2C+Age_Group%2C+Sex%2C+Hospitalization%2C+Outcome%2C+MT_case&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token="
 
 initial_pull <- GET(query_url)
@@ -38,28 +38,28 @@ state_data <- as.data.frame(json_data$features$attributes)
 reg1 <- as.data.frame(c("Carter", "Custer", "Daniels", "Dawson", "Fallon", "Garfield", "McCone",
                         "Phillips", "Powder River", "Prairie", "Richland", "Roosevelt", "Rosebud",
                         "Sheridan", "Treasure", "Valley", "Wibaux")) %>% 
-   rename(county = 1) %>% 
+   rename_at(1, ~"county") %>% 
    mutate(region = 1)
 
 reg2 <- as.data.frame(c("Blaine", "Cascade", "Chouteau", "Glacier", "Hill", "Liberty", "Pondera",
                         "Teton", "Toole")) %>% 
-   rename(county = 1) %>% 
+   rename_at(1, ~"county") %>% 
    mutate(region = 2)
 
 reg3 <- as.data.frame(c("Big Horn", "Carbon", "Fergus", "Golden Valley", "Judith Basin",
                         "Musselshell", "Petroleum", "Stillwater", "Sweet Grass", "Wheatland",
                         "Yellowstone")) %>% 
-   rename(county = 1) %>% 
+   rename_at(1, ~"county") %>% 
    mutate(region = 3)
 
 reg4 <- as.data.frame(c("Beaverhead", "Broadwater", "Deer Lodge", "Gallatin", "Granite", "Jefferson",
                         "Lewis and Clark", "Madison", "Meagher", "Park", "Powell", "Silver Bow")) %>% 
-   rename(county = 1) %>% 
+   rename_at(1, ~"county") %>% 
    mutate(region = 4)
 
 reg5 <- as.data.frame(c("Flathead", "Lake", "Lincoln", "Mineral", "Missoula", 
                         "Ravalli", "Sanders")) %>% 
-   rename(county = 1) %>% 
+   rename_at(1, ~"county") %>% 
    mutate(region = 5)
 
 counties_regions <- rbind(reg1, reg2, reg3, reg4, reg5)
@@ -72,7 +72,20 @@ state_data_clean <- state_data %>%
    mutate(dates = ymd(dates)) %>% 
    select(case_no, dates, county:mt_case) %>% 
    left_join(counties_regions, by = "county") %>% 
-   mutate(case = 1)
+   mutate(case = 1) %>% 
+   mutate(age_group2 = if_else(age_group == "80-89" | age_group == "90-99",
+                               "80+", age_group),
+          age_group_new = factor(age_group2, 
+                                 levels = c("0-9", "10-19", "20-29", 
+                                            "30-39", "40-49", "50-59", 
+                                            "60-69", "70-79", "80+"),
+                                 labels = c("0 to 9, 0.12", "10 to 19, 0.13", "20 to 29, 0.13", 
+                                            "30 to 39, 0.13", "40 to 49, 0.11", "50 to 59, 0.13", 
+                                            "60 to 69, 0.14", "70 to 79, 0.08", "80+, 0.04"))) %>% 
+   separate(age_group_new, c("age_group_new", "age_group_new_percent"), sep = ",") %>% 
+   mutate(age_group_new_percent = as.numeric(age_group_new_percent),
+          state_pop = as.numeric(1062000)) %>% 
+   select(-age_group2, -age_group_new, -age_group_new_percent, -state_pop)
 
 state_data_wide <- state_data_clean %>% 
    mutate(age_group = factor(age_group,
@@ -83,7 +96,7 @@ state_data_wide <- state_data_clean %>%
                                         "80 to 89 yrs", "90 to 99 yrs")),
           sex = factor(sex, labels = c("Female", "Male")),
           hospitalization = factor(hospitalization,
-                                   labels = c("Hosp: No", "Hosp: Possible",
+                                   labels = c("Hosp: No", "Hosp: Previous",
                                               "Hosp: Unknown", "Hosp: Yes"))) %>% 
    mutate(case = 1) %>% 
    pivot_wider(names_from = "age_group", values_from = "case") %>% 
@@ -97,74 +110,74 @@ state_data_wide <- state_data_clean %>%
 state_wide_date <- state_data_wide %>% 
    mutate(region = "state") %>% 
    group_by(dates) %>% 
-   mutate_at(c(6:50), sum, na.rm = TRUE) %>% 
+   mutate_at(c(6:52), sum, na.rm = TRUE) %>% 
    mutate(case = 1,
           daily_cases = sum(case)) %>% 
    arrange(dates) %>% 
    distinct(dates, .keep_all = TRUE) %>% 
    ungroup() %>% 
    mutate(cumulative_cases = cumsum(daily_cases)) %>% 
-   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Pondera)
+   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Fergus)
 
 reg1_wide_date <- state_data_wide %>% 
    filter(region == 1) %>% 
    group_by(dates) %>% 
-   mutate_at(c(6:50), sum, na.rm = TRUE) %>% 
+   mutate_at(c(6:52), sum, na.rm = TRUE) %>% 
    mutate(case = 1,
           daily_cases = sum(case)) %>% 
    arrange(dates) %>% 
    distinct(dates, .keep_all = TRUE) %>% 
    ungroup() %>% 
    mutate(cumulative_cases = cumsum(daily_cases)) %>% 
-   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Pondera)
+   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Fergus)
 
 reg2_wide_date <- state_data_wide %>% 
    filter(region == 2) %>% 
    group_by(dates) %>% 
-   mutate_at(c(6:50), sum, na.rm = TRUE) %>% 
+   mutate_at(c(6:52), sum, na.rm = TRUE) %>% 
    mutate(case = 1,
           daily_cases = sum(case)) %>% 
    arrange(dates) %>% 
    distinct(dates, .keep_all = TRUE) %>% 
    ungroup() %>% 
    mutate(cumulative_cases = cumsum(daily_cases)) %>% 
-   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Pondera)
+   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Fergus)
 
 reg3_wide_date <- state_data_wide %>% 
    filter(region == 3) %>% 
    group_by(dates) %>% 
-   mutate_at(c(6:50), sum, na.rm = TRUE) %>% 
+   mutate_at(c(6:52), sum, na.rm = TRUE) %>% 
    mutate(case = 1,
           daily_cases = sum(case)) %>% 
    arrange(dates) %>% 
    distinct(dates, .keep_all = TRUE) %>% 
    ungroup() %>% 
    mutate(cumulative_cases = cumsum(daily_cases)) %>% 
-   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Pondera)
+   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Fergus)
 
 reg4_wide_date <- state_data_wide %>% 
    filter(region == 4) %>% 
    group_by(dates) %>% 
-   mutate_at(c(6:50), sum, na.rm = TRUE) %>% 
+   mutate_at(c(6:52), sum, na.rm = TRUE) %>% 
    mutate(case = 1,
           daily_cases = sum(case)) %>% 
    arrange(dates) %>% 
    distinct(dates, .keep_all = TRUE) %>% 
    ungroup() %>% 
    mutate(cumulative_cases = cumsum(daily_cases)) %>% 
-   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Pondera)
+   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Fergus)
 
 reg5_wide_date <- state_data_wide %>% 
    filter(region == 5) %>% 
    group_by(dates) %>% 
-   mutate_at(c(6:50), sum, na.rm = TRUE) %>% 
+   mutate_at(c(6:52), sum, na.rm = TRUE) %>% 
    mutate(case = 1,
           daily_cases = sum(case)) %>% 
    arrange(dates) %>% 
    distinct(dates, .keep_all = TRUE) %>% 
    ungroup() %>% 
    mutate(cumulative_cases = cumsum(daily_cases)) %>% 
-   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Pondera)
+   select(region, dates, daily_cases, cumulative_cases, `70 to 79 yrs`:Fergus)
 
 all_data_wide <- rbind(state_wide_date, reg1_wide_date, reg2_wide_date,
                        reg3_wide_date, reg4_wide_date, reg5_wide_date)
@@ -183,7 +196,140 @@ state_data_incidence <- state_data %>%
    select(dates, I)
 
 
-# Run analysis and print results
+########## Function to get county-specific outcomes
+county_function <- function(county_name, data = state_data_clean){
+   
+   county_data <- data %>% 
+      filter(county == county_name)
+   
+   county_incidence <- incidence(county_data$dates, 
+                                 first_date = "2020-03-13", 
+                                 last_date = Sys.Date(),
+                                 standard = FALSE)
+   county_dates <- as.data.frame(county_incidence$dates) %>% 
+      rename_at(1, ~"dates")
+   county_cases <- county_incidence$counts
+   
+   county_data_new <<- county_dates %>% 
+      left_join(county_data, by = "dates") %>% 
+      mutate(case_new = case) %>% 
+      group_by(dates) %>% 
+      pivot_wider(names_from = "hospitalization", values_from = "case") %>% 
+      select(-"NA") %>% 
+      group_by(dates) %>% 
+      pivot_wider(names_from = "outcome", values_from = "case_new") %>% 
+      select(-"NA") %>% 
+      group_by(dates) %>% 
+      mutate_at(vars(one_of("N")), sum, na.rm = TRUE) %>% 
+      mutate_at(vars(one_of("Y")), sum, na.rm = TRUE) %>% 
+      mutate_at(vars(one_of("U")), sum, na.rm = TRUE) %>% 
+      mutate_at(vars(one_of("P")), sum, na.rm = TRUE) %>% 
+      mutate_at(vars(one_of("Active")), sum, na.rm = TRUE) %>% 
+      mutate_at(vars(one_of("Deceased")), sum, na.rm = TRUE) %>% 
+      mutate_at(vars(one_of("Recovered")), sum, na.rm = TRUE) %>% 
+      distinct(dates, .keep_all = TRUE) %>% 
+      ungroup() %>% 
+      fill(county, .direction = c("downup")) %>% 
+      fill(region, .direction = c("downup")) %>% 
+      select(-case_no, -age_group, -sex, -mt_case)
+   
+   
+   
+}
+
+missoula_county <- county_function("Missoula")
+gallatin_county <- county_function("Gallatin")
+yellowstone_county <- county_function("Yellowstone")
+richland_county <- county_function("Richland")
+roosevelt_county <- county_function("Roosevelt")
+cascade_county <- county_function("Cascade")
+glacier_county <- county_function("Glacier")
+hill_county <- county_function("Hill")
+liberty_county <- county_function("Liberty")
+pondera_county <- county_function("Pondera")
+toole_county <- county_function("Toole")
+bighorn_county <- county_function("Big Horn")
+carbon_county <- county_function("Carbon")
+fergus_county <- county_function("Fergus")
+gvalley_county <- county_function("Golden Valley")
+musselshell_county <- county_function("Musselshell")
+stillwater_county <- county_function("Stillwater")
+wheatland_county <- county_function("Wheatland")
+beaverhead_county <- county_function("Beaverhead")
+broadwater_county <- county_function("Broadwater")
+deerlodge_county <- county_function("Deer Lodge")
+jefferson_county <- county_function("Jefferson")
+lewisandclark_county <- county_function("Lewis and Clark")
+madison_county <- county_function("Madison")
+meagher_county <- county_function("Meagher")
+park_county <- county_function("Park")
+silverbow_county <- county_function("Silver Bow")
+flathead_county <- county_function("Flathead")
+lake_county <- county_function("Lake")
+lincoln_county <- county_function("Lincoln")
+ravalli_county <- county_function("Ravalli")
+
+
+county_data_combined <- plyr::rbind.fill(missoula_county, gallatin_county,
+                                         yellowstone_county, richland_county,
+                                         roosevelt_county, cascade_county,
+                                         glacier_county, hill_county,
+                                         liberty_county, pondera_county,
+                                         toole_county, bighorn_county,
+                                         carbon_county, fergus_county,
+                                         gvalley_county, musselshell_county,
+                                         stillwater_county, wheatland_county,
+                                         beaverhead_county, broadwater_county,
+                                         deerlodge_county, jefferson_county,
+                                         lewisandclark_county, madison_county,
+                                         meagher_county, park_county,
+                                         silverbow_county, flathead_county,
+                                         lake_county, lincoln_county,
+                                         ravalli_county) 
+
+write_csv(county_data_combined, "C:/R/covid19/state_daily_results/county_data_combined.csv", na = " ")
+
+
+#################### Run and save state hospitalization data
+state_hosp_data <- state_data %>% 
+   rename_all(tolower) %>% 
+   mutate(date_reported_to_cdepi = date_reported_to_cdepi/1000,
+          dates = as.POSIXct(date_reported_to_cdepi, origin = "1970-01-01")) %>% 
+   separate(dates, c("dates", "trash"), sep = " ") %>% 
+   mutate(dates = ymd(dates)) %>% 
+   select(case_no, dates, county:mt_case) %>% 
+   left_join(counties_regions, by = "county") %>% 
+   mutate(case = 1) %>% 
+   mutate(age_group2 = if_else(age_group == "80-89" | age_group == "90-99",
+                               "80+", age_group),
+          age_group_new = factor(age_group2, 
+                                 levels = c("0-9", "10-19", "20-29", 
+                                            "30-39", "40-49", "50-59", 
+                                            "60-69", "70-79", "80+"),
+                                 labels = c("0 to 9, 0.12", "10 to 19, 0.13", "20 to 29, 0.13", 
+                                            "30 to 39, 0.13", "40 to 49, 0.11", "50 to 59, 0.13", 
+                                            "60 to 69, 0.14", "70 to 79, 0.08", "80+, 0.04"))) %>% 
+   separate(age_group_new, c("age_group_new", "age_group_new_percent"), sep = ",") %>% 
+   mutate(age_group_new_percent = as.numeric(age_group_new_percent),
+          state_pop = as.numeric(1062000)) %>% 
+   select(-age_group2)
+
+state_hosp <- state_hosp_data %>% 
+   group_by(age_group_new) %>% 
+   mutate(age_group_n = sum(case)) %>% 
+   mutate(hosp = if_else(hospitalization == "Y" | hospitalization == "P", 1, 0),
+          hosp_n = sum(hosp, na.rm = TRUE),
+          hosp_perc = round(hosp_n/age_group_n*100, digits = 2)) %>% 
+   ungroup() %>% 
+   distinct(age_group_new, .keep_all = TRUE) %>% 
+   select(age_group_new, age_group_n, hosp_n, hosp_perc) %>% 
+   rename(age_group = age_group_new) %>% 
+   arrange(age_group)
+
+write_csv(state_hosp, "C:/R/covid19/state_daily_results/state_hosp.csv", na = " ")
+
+
+#################### Run analysis and print results
 serial_interval_mean <- 4
 serial_interval_sd <- 4.75
 
@@ -776,20 +922,17 @@ write_csv(all_regions_r, "C:/R/covid19/state_daily_results/all_regions_r.csv", n
 # password  = 'celftp'
 # remotepath = '/celFtpFiles/covid19/Rt/incoming/'
 
-sftp_con <- sftp_connect(server = "elbastion.dbs.umt.edu",
-                         folder = "/celFtpFiles/covid19/Rt/incoming",
-                         username = "celftp",
-                         password = "celftp")
-
-sftp_upload(file = "all_regions_r.csv", 
-            fromfolder = "C:/R/covid19/state_daily_results/",
-            sftp_connection = sftp_con)
-
-
-
 sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
            "/celFtpFiles/covid19/Rt/incoming/all_regions_r.csv",
            "C:/R/covid19/state_daily_results/all_regions_r.csv")
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/state_hosp.csv",
+           "C:/R/covid19/state_daily_results/state_hosp.csv")
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/county_data_combined.csv",
+           "C:/R/covid19/state_daily_results/county_data_combined.csv")
 
 sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
            "/celFtpFiles/covid19/Rt/incoming/state_r_plot.png",
