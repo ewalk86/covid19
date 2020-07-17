38c0e38bc93b225c3568647b4046e87744a1ef64
ewalk86 <ethanwalker86@gmail.com>
2020-07-17 16:47:35 -0600

    state and county updates

--- county_r_estimates.R
+++ county_r_estimates.R
@@ -667,9 +667,288 @@ ggsave("C:/R/covid19/bighorn_inc_plot.png", width = 10, height = 8)
 
 
 
+## Lake county results
+# Format analysis data
+lake_li_analysis_data <- mt_case_data %>% 
+   filter(county == "Lake") %>% 
+   group_by(dates) %>% 
+   mutate(local = sum(local),
+          imported = sum(imported)) %>% 
+   select(dates, local, imported) %>% 
+   distinct(dates, .keep_all = TRUE) %>% 
+   arrange(dates)
+
+
+lake_data <- lake_li_analysis_data 
+
+inc_data <- lake_data %>% 
+   mutate(I = local + imported) %>% 
+   select(dates, I) 
+
+latest_date <- format(Sys.Date() - 14, "%Y-%m-%d")
+
+lake_inc_data <- incidence(inc_data$dates, last_date = latest_date)
+
+lake_li_inc_data <- as.data.frame(lake_inc_data$dates) %>% 
+   mutate(dates = ymd(lake_inc_data$dates)) %>% 
+   select(dates) %>% 
+   left_join(lake_data, by = "dates") %>% 
+   mutate(local = if_else(is.na(local), 0, local),
+          imported = if_else(is.na(imported), 0, imported)) 
+
+
+lake_n <- as.data.frame(lake_li_inc_data$dates)
+time_var <- nrow(lake_n)
+time_start <- seq(2, time_var-13)
+time_end <- time_start + 13
+
+
+MCMC_seed <- 1
+overall_seed <- 2
+mcmc_control <- make_mcmc_control(seed = MCMC_seed, burnin = 1000, thin = 10)
+dist <- "G"  # fitting a Gamma distribution for the SI
+si_config <- make_config(list(si_parametric_distr = dist, 
+                              mcmc_control = mcmc_control, seed = overall_seed, n1 = 500, n2 = 50,
+                              t_start = time_start, t_end = time_end))
+
+lake_r_results <- estimate_R(lake_li_inc_data, method = "si_from_data", 
+                                si_data = mt_si_data, config = si_config)
+
+
+
+colomns <- c(1:4, 8)
+lake_r <- (lake_r_results$R[,colomns]) %>% 
+   mutate(region = "Lake") %>% 
+   rename(mean_r = `Mean(R)`,
+          sd_r = `Std(R)`,
+          median_r = `Median(R)`)
+
+lake_dates <- as.data.frame(lake_r_results$dates)
+lake_i <- as.data.frame(lake_r_results$I)
+lake_cil <- as.data.frame(lake_r_results$R$`Quantile.0.025(R)`)
+lake_cih <- as.data.frame(lake_r_results$R$`Quantile.0.975(R)`)
+lake_dates_new <- cbind(lake_dates, lake_i) %>% 
+   rename(dates = 1,
+          incidence = 2) %>% 
+   mutate(dates = ymd(dates))
+lake_dates_new <- lake_dates_new[-(1:14), 1:2]
+lake_dates_new <- cbind(lake_dates_new, lake_cil, lake_cih) %>% 
+   rename(cl_low = 3,
+          cl_high = 4)
+lake_r_clean <- cbind(lake_r, lake_dates_new)
+
+lake_data_clean <- mt_case_data %>% 
+   filter(county == "Lake")
+total_cases <- sum(lake_data_clean$case)
+
+
+lake_r_plot <- lake_r_clean %>% 
+   filter(dates > Sys.Date() - 35) %>% 
+   ggplot() +
+   geom_line(aes(dates, mean_r), size = 1.5, color = "black") +
+   geom_line(aes(dates, cl_low), size = 1.5, color = "grey") +
+   geom_line(aes(dates, cl_high), size = 1.5, color = "grey") +
+   labs(title = "COVID-19 Rolling 14-day R-values, Lake County, 2020",
+        color = "") +
+   ylab("R-value") +
+   xlab("") +
+   geom_hline(yintercept = 1, color = "red", size = 1.2) +
+   scale_x_date(date_breaks = "1 days", date_labels = "%d-%b") +
+   #scale_y_continuous(breaks = seq(0, 10, 0.5), labels = seq(0, 10, 0.5)) +
+   theme_minimal() +
+   theme(strip.text = element_text(size = 16, colour = "black"),
+         title = element_text(size = 12, colour = "black"),
+         panel.grid = element_blank(),
+         panel.grid.major.y = element_line(colour = "grey"),
+         axis.text.x = element_text(size = 12, colour = "black", 
+                                    angle = 90, vjust = 0.4),
+         axis.text.y = element_text(size = 12, colour = "black"),
+         legend.text = element_text(size = 12, colour = "black"),
+         axis.title.y = element_text(size = 12, colour = "black",
+                                     margin = unit(c(0, 5, 0, 0), "mm")),
+         axis.title.x = element_text(size = 12, colour = "black",
+                                     margin = unit(c(5, 0, 0, 0), "mm")),
+         axis.line.x = element_blank(), 
+         axis.line.y = element_blank(), 
+         axis.ticks = element_blank()) +
+   scale_color_manual(values = c("black")) 
+#lake_r_plot
+ggsave("C:/R/covid19/lake_r_plot.png", width = 10, height = 8)
+
+lake_inc_plot <- lake_data_clean %>% 
+   ggplot() +
+   geom_col(aes(dates, case), 
+            fill = "steelblue") +
+   labs(title = paste0("COVID-19 Cases in Lake County, 2020", 
+                       " (Total cases = ", total_cases, ")")) +
+   ylab("Number of Cases") +
+   xlab("") +
+   scale_x_date(date_breaks = "3 day", date_labels = "%d-%b") +
+   theme_minimal() +
+   theme(strip.text = element_text(size = 16, colour = "black"),
+         title = element_text(size = 12, colour = "black"),
+         panel.grid = element_blank(),
+         panel.grid.major.y = element_line(colour = "grey"),
+         axis.text.x = element_text(size = 12, colour = "black", 
+                                    angle = 90, vjust = 0.4),
+         axis.text.y = element_text(size = 12, colour = "black"),
+         axis.title.y = element_text(size = 12, colour = "black",
+                                     margin = unit(c(0, 5, 0, 0), "mm")),
+         axis.title.x = element_text(size = 12, colour = "black",
+                                     margin = unit(c(5, 0, 0, 0), "mm")),
+         axis.line.x = element_blank(), 
+         axis.line.y = element_blank(), 
+         axis.ticks = element_blank()) 
+#lake_inc_plot
+
+ggsave("C:/R/covid19/lake_inc_plot.png", width = 10, height = 8)
+
+
+
+
+## Lewis and Clark county results
+# Format analysis data
+lewisandclark_li_analysis_data <- mt_case_data %>% 
+   filter(county == "Lewis and Clark") %>% 
+   group_by(dates) %>% 
+   mutate(local = sum(local),
+          imported = sum(imported)) %>% 
+   select(dates, local, imported) %>% 
+   distinct(dates, .keep_all = TRUE) %>% 
+   arrange(dates)
+
+
+lewisandclark_data <- lewisandclark_li_analysis_data 
+
+inc_data <- lewisandclark_data %>% 
+   mutate(I = local + imported) %>% 
+   select(dates, I) 
+
+latest_date <- format(Sys.Date() - 14, "%Y-%m-%d")
+
+lewisandclark_inc_data <- incidence(inc_data$dates, last_date = latest_date)
+
+lewisandclark_li_inc_data <- as.data.frame(lewisandclark_inc_data$dates) %>% 
+   mutate(dates = ymd(lewisandclark_inc_data$dates)) %>% 
+   select(dates) %>% 
+   left_join(lewisandclark_data, by = "dates") %>% 
+   mutate(local = if_else(is.na(local), 0, local),
+          imported = if_else(is.na(imported), 0, imported)) 
+
+
+lewisandclark_n <- as.data.frame(lewisandclark_li_inc_data$dates)
+time_var <- nrow(lewisandclark_n)
+time_start <- seq(2, time_var-13)
+time_end <- time_start + 13
+
+
+MCMC_seed <- 1
+overall_seed <- 2
+mcmc_control <- make_mcmc_control(seed = MCMC_seed, burnin = 1000, thin = 10)
+dist <- "G"  # fitting a Gamma distribution for the SI
+si_config <- make_config(list(si_parametric_distr = dist, 
+                              mcmc_control = mcmc_control, seed = overall_seed, n1 = 500, n2 = 50,
+                              t_start = time_start, t_end = time_end))
+
+lewisandclark_r_results <- estimate_R(lewisandclark_li_inc_data, method = "si_from_data", 
+                                si_data = mt_si_data, config = si_config)
+
+
+
+colomns <- c(1:4, 8)
+lewisandclark_r <- (lewisandclark_r_results$R[,colomns]) %>% 
+   mutate(region = "Lewis and Clark") %>% 
+   rename(mean_r = `Mean(R)`,
+          sd_r = `Std(R)`,
+          median_r = `Median(R)`)
+
+lewisandclark_dates <- as.data.frame(lewisandclark_r_results$dates)
+lewisandclark_i <- as.data.frame(lewisandclark_r_results$I)
+lewisandclark_cil <- as.data.frame(lewisandclark_r_results$R$`Quantile.0.025(R)`)
+lewisandclark_cih <- as.data.frame(lewisandclark_r_results$R$`Quantile.0.975(R)`)
+lewisandclark_dates_new <- cbind(lewisandclark_dates, lewisandclark_i) %>% 
+   rename(dates = 1,
+          incidence = 2) %>% 
+   mutate(dates = ymd(dates))
+lewisandclark_dates_new <- lewisandclark_dates_new[-(1:14), 1:2]
+lewisandclark_dates_new <- cbind(lewisandclark_dates_new, lewisandclark_cil, lewisandclark_cih) %>% 
+   rename(cl_low = 3,
+          cl_high = 4)
+lewisandclark_r_clean <- cbind(lewisandclark_r, lewisandclark_dates_new)
+
+lewisandclark_data_clean <- mt_case_data %>% 
+   filter(county == "Lewis and Clark")
+total_cases <- sum(lewisandclark_data_clean$case)
+
+
+lewisandclark_r_plot <- lewisandclark_r_clean %>% 
+   filter(dates > Sys.Date() - 35) %>% 
+   ggplot() +
+   geom_line(aes(dates, mean_r), size = 1.5, color = "black") +
+   geom_line(aes(dates, cl_low), size = 1.5, color = "grey") +
+   geom_line(aes(dates, cl_high), size = 1.5, color = "grey") +
+   labs(title = "COVID-19 Rolling 14-day R-values, Lewis and Clark County, 2020",
+        color = "") +
+   ylab("R-value") +
+   xlab("") +
+   geom_hline(yintercept = 1, color = "red", size = 1.2) +
+   scale_x_date(date_breaks = "1 days", date_labels = "%d-%b") +
+   #scale_y_continuous(breaks = seq(0, 10, 0.5), labels = seq(0, 10, 0.5)) +
+   theme_minimal() +
+   theme(strip.text = element_text(size = 16, colour = "black"),
+         title = element_text(size = 12, colour = "black"),
+         panel.grid = element_blank(),
+         panel.grid.major.y = element_line(colour = "grey"),
+         axis.text.x = element_text(size = 12, colour = "black", 
+                                    angle = 90, vjust = 0.4),
+         axis.text.y = element_text(size = 12, colour = "black"),
+         legend.text = element_text(size = 12, colour = "black"),
+         axis.title.y = element_text(size = 12, colour = "black",
+                                     margin = unit(c(0, 5, 0, 0), "mm")),
+         axis.title.x = element_text(size = 12, colour = "black",
+                                     margin = unit(c(5, 0, 0, 0), "mm")),
+         axis.line.x = element_blank(), 
+         axis.line.y = element_blank(), 
+         axis.ticks = element_blank()) +
+   scale_color_manual(values = c("black")) 
+#lewisandclark_r_plot
+ggsave("C:/R/covid19/lewisandclark_r_plot.png", width = 10, height = 8)
+
+lewisandclark_inc_plot <- lewisandclark_data_clean %>% 
+   ggplot() +
+   geom_col(aes(dates, case), 
+            fill = "steelblue") +
+   labs(title = paste0("COVID-19 Cases in Lewis and Clark County, 2020", 
+                       " (Total cases = ", total_cases, ")")) +
+   ylab("Number of Cases") +
+   xlab("") +
+   scale_x_date(date_breaks = "3 day", date_labels = "%d-%b") +
+   theme_minimal() +
+   theme(strip.text = element_text(size = 16, colour = "black"),
+         title = element_text(size = 12, colour = "black"),
+         panel.grid = element_blank(),
+         panel.grid.major.y = element_line(colour = "grey"),
+         axis.text.x = element_text(size = 12, colour = "black", 
+                                    angle = 90, vjust = 0.4),
+         axis.text.y = element_text(size = 12, colour = "black"),
+         axis.title.y = element_text(size = 12, colour = "black",
+                                     margin = unit(c(0, 5, 0, 0), "mm")),
+         axis.title.x = element_text(size = 12, colour = "black",
+                                     margin = unit(c(5, 0, 0, 0), "mm")),
+         axis.line.x = element_blank(), 
+         axis.line.y = element_blank(), 
+         axis.ticks = element_blank()) 
+#lewisandclark_inc_plot
+
+ggsave("C:/R/covid19/lewisandclark_inc_plot.png", width = 10, height = 8)
+
+
+
+
 
 # Bind files and save
-all_counties_r <- rbind(missoula_r_clean, gallatin_r_clean, yellowstone_r_clean, bighorn_r_clean) 
+all_counties_r <- rbind(missoula_r_clean, gallatin_r_clean, yellowstone_r_clean, 
+                        bighorn_r_clean, lake_r_clean, lewisandclark_r_clean) 
 
 write_csv(all_counties_r, "C:/R/covid19/all_counties_r.csv", na = " ")
 
@@ -684,3 +963,19 @@ sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
            "/celFtpFiles/covid19/Rt/incoming/all_counties_r.csv",
            "C:/R/covid19/all_counties_r.csv")
 
+
+# Combine county R file and region/state R file; push to server
+
+all_counties_r <- read_csv("C:/R/covid19/all_counties_r.csv")
+
+all_regions_r <- read_csv("C:/R/covid19/state_daily_results/all_regions_r.csv")
+
+all_regions_cos_r <- plyr::rbind.fill(all_counties_r, all_regions_r)
+
+
+write_csv(all_regions_cos_r, "C:/R/covid19/all_regions_cos_r.csv", na = " ")
+
+
+sftpUpload("elbastion.dbs.umt.edu", "celftp", "celftp",
+           "/celFtpFiles/covid19/Rt/incoming/all_regions_cos_r.csv",
+           "C:/R/covid19/all_regions_cos_r.csv")
